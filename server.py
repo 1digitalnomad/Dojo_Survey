d95168bac01d75e10326b0a9acbb91cc4b3cd645
Raul <Takethefirststep@Rauls-iMac.attlocal.net>
2018-10-22 23:56:02 -0700

    survey with validations

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask, render_template, redirect, request, session, flash
 app = Flask(__name__)
+app.secret_key = 'DoNotTell'
 
 @app.route("/")
 def home():
@@ -8,9 +9,28 @@ def home():
 
 @app.route('/result', methods=['POST'])
 def result():
+    errors=False
+    session['your_name']=request.form['your_name']
+    session['location']=request.form['location']
+    session['language']=request.form['language']
+    session['comments']=request.form['comments']
+
+    if len(request.form['your_name']) < 1:
+        flash('Name can not be empty. Try again.')
+        errors=True
+    if len(request.form['comments'])<1:
+        flash('Comments Can Not Be Empty')
+        errors=True
+
+    if errors==True:
+        return redirect("/")
+    else:
+        return render_template('result.html')
+
+
+
     print('The result page')
 
-    return render_template('result.html')
 
 @app.route('/danger')
 def dang():
